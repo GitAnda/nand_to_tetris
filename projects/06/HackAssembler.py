4cec4be3f57b415bde06afbc8af7c6190a46fcee
GitAnda <andagfilipovic@gmail.com>
2022-08-31 08:31:25 +0200

    improve hack assembler (has bugs)

--- projects/06/HackAssembler.py
+++ projects/06/HackAssembler.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
+import re
 
 
 class HackAssembler():
@@ -11,17 +12,44 @@ class HackAssembler():
         self.jump_symbols = dict()
         self.instructions = self.set_instructions()
 
-        self.lookup_comp = None
+        with open('comp') as f:
+            self.lookup_comp = {}
+            for line in f.readlines():
+                s, b = line.strip().split(' ')
+                self.lookup_comp[s] = b
+        with open('jump') as f:
+            self.lookup_jump = {}
+            for line in f.readlines():
+                s, b = line.strip().split(' ')
+                self.lookup_jump[s] = b
 
         self.machine_code = self.get_machine_code()
 
-    def convert(self, line):
-        pass
+    @staticmethod
+    def split_c_instruction(str_instruction):
+        res = re.split('=|;', str_instruction)
+        if '=' not in str_instruction:
+            res.insert(0, 'null')
+        if ';' not in str_instruction:
+            res.append('null')
+        return res
     
     def get_machine_code(self):
+        code = []
         for instruction in self.instructions:
+            if instruction[0] == '@':
+                v = self.register_symbols[instruction[1:]]
+                machine_code = f'0{v:15x}'.replace(' ', '0')
 
-        pass
+            else:
+                d, c, j = self.split_c_instruction(instruction)
+                c = self.lookup_comp[c]
+                d = f"{int('A' in d)}{int('D' in d)}{int('M' in d)}"
+                j = self.lookup_jump[j]
+                machine_code = f'111{c}{d}{j}'
+                print(instruction, machine_code)
+            code.append(machine_code)
+        return code
 
     def find_next_register(self):
         while self.available_register in self.register_symbols.values():
@@ -61,6 +89,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     assembler = HackAssembler(args.path)
+
+    # for s, b in zip(assembler.instructions, assembler.machine_code):
+    #     print(f"{s}, {b}")
 
     exit(0)
 
